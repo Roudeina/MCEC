13dd9ba01d63962686cdfa96d6ca032131d125f4
Roudeina Ben Trad <roudeina.bentrad@gmail.com>
2021-01-15 19:54:01 +0100

    addsign&log

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ProfileComponent } from './component/profile/profile.component';
-import { RegisterComponent} from './component/profile/register/register.component';
+import { RegisterComponent} from './component/register/register.component';
 import {BecomeAhostComponent} from './component/become-ahost/become-ahost.component'
-
+import {LoginComponent} from './component/login/login.component'
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path:'register', component: RegisterComponent},
+  { path:'login', component: LoginComponent},
   { path:'profile', component: ProfileComponent},
   { path:'becomeAhost', component: BecomeAhostComponent}
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import {MatRadioModule} from '@angular/material/radio';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import {HttpClientModule} from "@angular/common/http"
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './component/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,21 +14,23 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './component/home/home.component';
-import { ProfileComponent } from './component/profile/profile.component';
-import { RegisterComponent } from './component/profile/register/register.component';
-import { BecomeAhostComponent } from './component/become-ahost/become-ahost.component';
-import { MaterialFileUploadComponent } from './component/become-ahost/material-file-upload/material-file-upload.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './component/home/home.component';
+import { RegisterComponent } from './component/register/register.component';
+import { BecomeAhostComponent } from './component/become-ahost/become-ahost.component';
+import { MaterialFileUploadComponent } from './component/become-ahost/material-file-upload/material-file-upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    ProfileComponent,
     RegisterComponent,
     BecomeAhostComponent,
     MaterialFileUploadComponent
@@ -38,6 +38,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
