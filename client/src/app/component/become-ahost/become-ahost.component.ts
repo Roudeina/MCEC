b8d5066ede42c7f476567cf69d265516bf947e1c
Roudeina Ben Trad <roudeina.bentrad@gmail.com>
2021-01-16 11:37:35 +0100

    becomeahost

--- client/src/app/component/become-ahost/become-ahost.component.ts
+++ client/src/app/component/become-ahost/become-ahost.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
-import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-// import { UploadService } from  '../upload.service';
-
+import {HttpClient} from '@angular/common/http';
+import {HostRegister} from './host-register'
+import {EnrolService} from './enrol.service'
 
 @Component({
   selector: 'app-become-ahost',
   templateUrl: './become-ahost.component.html',
   styleUrls: ['./become-ahost.component.css']
 })
-export class BecomeAhostComponent implements OnInit {
+export class BecomeAhostComponent {
+  selectedFile= File=null;
+  fd=new FormData();
+  hostModel= new HostRegister("",false,this.selectedFile)
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor(private _enrolService:EnrolService, http:HttpClient){}
+  onFileSelected(event){
+    this.selectedFile=<File>event.target.files[0];
+    console.log('selectedFile',this.selectedFile)
   }
 
-}
+  onSubmit(){
+    console.log('data to be sent',this.hostModel)
+    this._enrolService.enroll(this.hostModel)
+    .subscribe(
+      data =>console.log('success',data),
+      err => console.log('error!',err)
+    )
+  }
+  }
---
+++ client/src/app/component/become-ahost/host-register.ts
@@ -0,0 +1,8 @@
+export class HostRegister {
+    constructor(
+        public typeOfShare: string,
+        public occupied: boolean,
+        public image: File,
+    ){}
+
+}
